479c6d11dfdf02a05bc9001e235b6db7a1187e5b
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2025-02-03 08:26:03 +0100

    version one

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
   locales: ["en", "fr"],
-  defaultLocale: "en",
+  defaultLocale: "fr",
 });
 
 export const config = {
